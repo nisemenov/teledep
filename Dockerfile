3299857f1de81af0bb50e78252ae3583c967fc16
Nikita Semenov <nasemenov726@gmail.com>
2024-06-05 11:43:29 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,4 @@ FROM python:3.12.3
 
 WORKDIR /app
 
-COPY main.py /app/main.py
-COPY .env /app/.env
-COPY pyproject.toml /app/
-
-RUN pip install poetry && poetry config virtualenvs.create false && poetry install
-
-# CMD ["python", "main.py"]
+COPY . .
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import subprocess
 load_dotenv()
 
 TOKEN: str = os.getenv('TOKEN')
+path: str = '/app/teledep'
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,6 +17,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         'Команды для git:\n'
         '- /abort - отмена merge в случае конфликта при pull;\n'
+        '- /log - просмотр 10 последних логов;\n'
         '- /pull - обновление проекта;\n'
         'Команды для docker:\n'
         '- /down - docker compose down\n'
@@ -26,28 +28,41 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def pull(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Pulling from remote origin...')
     
-    result = subprocess.run("git pull", shell=True, capture_output=True, text=True)
+    result = subprocess.run(
+        f'cd {path} && git pull', 
+        shell=True, 
+        capture_output=True, 
+        text=True
+    )
     if result.returncode == 0:
         await update.message.reply_text(result.stdout)
     else:
         await update.message.reply_text(f'{result.stdout}\n{result.stderr}')
 
 async def abort(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    result = subprocess.run("git merge --abort", shell=True, capture_output=True, text=True)
-    await update.message.reply_text('Merge conflict отменен')
+    result = subprocess.run(
+        f'cd {path} && git merge --abort', 
+        shell=True, 
+        capture_output=True, 
+        text=True
+    )
+    await update.message.reply_text('Merge отменен')
+
+async def log(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    result = subprocess.run(
+        f'cd {path} && git log --oneline --decorate --graph --all -n 10', 
+        shell=True, 
+        capture_output=True, 
+        text=True
+    )
+    await update.message.reply_text('10 последних логов' + result.stdout)
 
 # for docker
 async def down(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     pass
 
 def main() -> None:
-    application = Application.builder().token(TOKEN).build()
-
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('abort', abort))
-    application.add_handler(CommandHandler('pull', pull))
-
-    application.run_polling()
+    pass
 
 if __name__ == '__main__':
     main()
