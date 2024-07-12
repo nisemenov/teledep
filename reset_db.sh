bf78e86650d03508291466a5c72d2ddb1c4f31bc
Nikita Semenov <nasemenov726@gmail.com>
2024-07-12 10:51:34 +0300

    hotfix

--- reset_db.sh
+++ reset_db.sh
@@ -3,6 +3,6 @@
 source .env
 
 docker stop $APP_CONTAINER
-docker exec -it $POSTGRES_CONTAINER psql -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_NAME;" -p $DB_PORT
-docker exec -it $POSTGRES_CONTAINER psql -U $DB_USER -c "CREATE DATABASE $DB_NAME;" -p $DB_PORT
+docker exec $POSTGRES_CONTAINER psql -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_NAME;" -p $DB_PORT
+docker exec $POSTGRES_CONTAINER psql -U $DB_USER -c "CREATE DATABASE $DB_NAME;" -p $DB_PORT
 docker start $APP_CONTAINER
