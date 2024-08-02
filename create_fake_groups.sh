283b06f3fd708ecdda3a661e7641dcc0b0cf8676
Nikita Semenov <nasemenov726@gmail.com>
2024-08-02 15:39:50 +0300

    devided create_fake_profiles and added create_fake_groups

---
+++ create_fake_groups.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source .env
+
+docker exec $APP_CONTAINER python3 manage.py create_fake_groups
--- create_fake_profiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .env
-
-docker exec $APP_CONTAINER python3 manage.py profilefactory
